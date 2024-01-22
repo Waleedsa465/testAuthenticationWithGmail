02d9de239a4b6bb4d60a812de1643ca5c5ef65ab
Waleedsa465 <waleedsa465@gmail.com>
2024-01-22 17:54:44 +0500

    Project Update

--- Pods/Target Support Files/Pods-BirdsApps/Pods-BirdsApps-umbrella.h
+++ Pods/Target Support Files/Pods-BirdsApps/Pods-BirdsApps-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_testVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_testVersionString[];
+FOUNDATION_EXPORT double Pods_BirdsAppsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BirdsAppsVersionString[];
 
